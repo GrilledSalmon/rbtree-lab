ccf395cb8aa7a2d631bfc65b9c35163d84a180b1
yoonwoo_labtop <mryoonwoo@naver.com>
2022-05-03 16:10:13 +0900

    mid commit

--- src/rbtree.c
+++ src/rbtree.c
@@ -8,13 +8,84 @@ rbtree *new_rbtree(void) {
   return p;
 }
 
+/* 수정할 RBtree와 서브트리의 루트(x)를 받아옴 */
+void left_rotate(rbtree *t, node_t *x) {
+  node_t *y = x->right;
+  x->right = y->left;
+  if (y->left != t->nil)
+    y->left->parent = x;
+  
+  y->parent = x->parent;
+
+  if (x->parent == t->nil)
+    t->root = y;
+  else if (x == x->parent->left)
+    x->parent->left = y;
+  else
+    x->parent->right = y;
+
+  y->left = x;
+  x->parent = y;
+}
+
+// left_rotate에서 left, right 자리만 바꿔서 만듦
+void right_rotate(rbtree *t, node_t *x) {
+  node_t *y = x->left;
+  x->left = y->right;
+  if (y->right != t->nil)
+    y->right->parent = x;
+  
+  y->parent = x->parent;
+
+  if (x->parent == t->nil)
+    t->root = y;
+  else if (x == x->parent->right)
+    x->parent->right = y;
+  else
+    x->parent->left = y;
+
+  y->right = x;
+  x->parent = y;
+}
+
+
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
   free(t);
 }
 
+void rbtree_insert_fixup(rbtree *t, node_t *z) {
+
+}
+
+
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
+  node_t *z = (node_t *)malloc(sizeof(node_t)); // 삽입할 노드
+  z->color = RBTREE_RED;
+  z->key = key;
+  z->left = z->right = t->nil;
+
+  node_t *y = t->nil; // 삽입될 노드의 부모
+  node_t *x = t->root; // temp..?
+  
+  while (x != t->nil){
+    y = x;
+    if (z->key < x->key)
+      x = x->left;
+    else
+      x = x->right;
+  }
+  z->parent = y; // 부모 관계 설정
+  if (y==t->nil)
+    t->root = z;
+  else if (z->key < y->key)
+    y->left = z;
+  else
+    y->right = z;
+  
+  rbtree_insert_fixup(t, z);
+
   return t->root;
 }
 
@@ -43,8 +114,3 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   return 0;
 }
 
-int main()
-{
-  printf("hello?\n");
-  return 0;
-}
